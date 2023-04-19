fb096a8898aaf3c9f80ab65e8f99b4041c5e2596
Stasoon <stas.csa@ya.ru>
2023-04-20 01:52:55 +0300

    FINAL

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// подключаем Dotenv.
+ // подключаем Dotenv.
 require('dotenv').config();
 const TelegramAPI = require('node-telegram-bot-api') // фреймворк для работы с API Telegram
 const keyboard =  require ('./module/keyboard')
@@ -248,7 +248,6 @@ bot.onText(/\/ADMINnamebutton/, async msg => {
     
 })
 
-bot.onText(/\/linkbutton/, async msg => {
 bot.onText(/\/ADMINlinkbutton/, async msg => {
     LinkButton = msg.text.split(" ").slice(1,2).join(" ");
 
@@ -300,8 +299,7 @@ bot.onText(/\/ADMINusers/, async msg => {
     const ChatId = msg.chat.id;
     const log = await User.find( { }, { ChatId: 1, _id: 0 } );
 
-    bot.sendMessage(ChatId, `Ботом воспользовалиось ${log.length} человек.`, {
     bot.sendMessage(ChatId, `Ботом воспользовались ${log.length} человек.`, {
-        parse_mode: 'HTML',
+        parse_mode: 'HTML'
     })
 })
